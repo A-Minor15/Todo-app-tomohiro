1077eb4f2df3c7ceb6c2c06c24206c1f3262c718
A-Minor15 <degtju15@outlook.jp>
2024-02-04 21:57:57 +0900

    Todo一覧取得APIの作成

---
+++ app/Models/Todo.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    use HasFactory;
+
+
+    public function getTodos() {
+        $todos = $this->select('id', 'name', 'description', 'priority', 'expires_at', 'created_at')->get();
+
+        return $todos;
+    }
+}
